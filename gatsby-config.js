90562291ed508087927ccd6d4dc2639ceb9c2a21
Luke Taylor <lmcjt@outlook.com>
2018-07-05 19:43:30 +0100

    removed unnecessary files and fixed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: "L3 Digital",
-    author: "L3 Digital",
-    description: "Website for L3 Digital"
+    title: 'L3 Digital',
+    author: 'L3 Digital',
+    description: 'Website for L3 Digital',
   },
   pathPrefix: '/',
   plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/posts`,
-        name: "posts",
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     path: `${__dirname}/src/posts`,
+    //     name: "posts",
+    //   },
+    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -30,13 +30,13 @@ module.exports = {
               maxWidth: 630,
             },
           },
-          "gatsby-remark-copy-linked-files",
+          'gatsby-remark-copy-linked-files',
         ],
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
   ],
 }
