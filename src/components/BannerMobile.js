8035c177dbf615a297f2c8221b99fb055085ba3b
Luke Taylor <lmcjt@outlook.com>
2018-07-09 08:41:57 +0100

    added mobile only banner

---
+++ src/components/BannerMobile.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const BannerMobile = props => (
+  <section id="banner" className="major">
+    <div className="inner">
+      <header className="major">
+        <h1>We Are L3 Digital</h1>
+      </header>
+      <div className="content">
+        <p>
+          A team of great collaborators, providing solutions to all your
+          development needs.
+        </p>
+        <ul className="actions">
+          <li>
+            <a href="#one" className="button next">
+              Learn more
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+)
+export default BannerMobile
