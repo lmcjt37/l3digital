8f8f0bb9818fb651484e84771ec23d9d2e787210
Luke Taylor <lmcjt@outlook.com>
2019-02-05 17:49:30 +0000

    fixed

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -11,81 +11,80 @@ import Contact from '../components/Contact'
 import Footer from '../components/Footer'
 
 class Template extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isMenuVisible: false,
-      loading: 'is-loading',
+    constructor(props) {
+        super(props)
+        this.state = {
+            isMenuVisible: false,
+            loading: 'is-loading',
+        }
+        this.handleToggleMenu = this.handleToggleMenu.bind(this)
     }
-    this.handleToggleMenu = this.handleToggleMenu.bind(this)
-  }
 
-  componentDidMount() {
-    this.timeoutId = setTimeout(() => {
-      this.setState({ loading: '' })
+    componentDidMount() {
+        this.timeoutId = setTimeout(() => {
+            this.setState({ loading: '' })
 
-      // TODO:: fix scroll
-      // let section = document.querySelectorAll('.smooth-scroll-section'),
-      //   sections = {}
-      //
-      // Array.prototype.forEach.call(section, e => {
-      //   sections[e.id] = {
-      //     start: e.offsetTop,
-      //     end: e.offsetTop + e.offsetHeight,
-      //   }
-      //
-      //   document
-      //     .querySelector(`a[href*='${e.id}']:not([href='#']`)
-      //     .addEventListener('click', evt => {
-      //       window.scroll({
-      //         top: sections[e.id].start,
-      //         left: 0,
-      //         behavior: 'smooth',
-      //       })
-      //       evt.preventDefault()
-      //     })
-      // })
-    }, 100)
-  }
-
-  componentWillUnmount() {
-    if (this.timeoutId) {
-      clearTimeout(this.timeoutId)
+            let section = document.querySelectorAll('.smooth-scroll-section'),
+                sections = {}
+            Array.prototype.forEach.call(section, e => {
+                sections[e.id] = {
+                    start: e.offsetTop,
+                    end: e.offsetTop + e.offsetHeight,
+                }
+                document
+                    .querySelector(
+                        `a[href*='${e.id}']:not([href='#']):not([href='#0'])`
+                    )
+                    .addEventListener('click', evt => {
+                        window.scroll({
+                            top: sections[e.id].start,
+                            left: 0,
+                            behavior: 'smooth',
+                        })
+                        evt.preventDefault()
+                    })
+            })
+        }, 100)
     }
-  }
 
-  handleToggleMenu() {
-    this.setState({
-      isMenuVisible: !this.state.isMenuVisible,
-    })
-  }
+    componentWillUnmount() {
+        if (this.timeoutId) {
+            clearTimeout(this.timeoutId)
+        }
+    }
 
-  render() {
-    const { children } = this.props
+    handleToggleMenu() {
+        this.setState({
+            isMenuVisible: !this.state.isMenuVisible,
+        })
+    }
 
-    return (
-      <div
-        className={`body ${this.state.loading} ${
-          this.state.isMenuVisible ? 'is-menu-visible' : ''
-        }`}
-      >
-        <Helmet>
-          <link rel="stylesheet" href={withPrefix('skel.css')} />
-        </Helmet>
-        <div id="wrapper">
-          <Header onToggleMenu={this.handleToggleMenu} />
-          {children()}
-          <Contact />
-          <Footer />
-        </div>
-        <Menu onToggleMenu={this.handleToggleMenu} />
-      </div>
-    )
-  }
+    render() {
+        const { children } = this.props
+
+        return (
+            <div
+                className={`body ${this.state.loading} ${
+                    this.state.isMenuVisible ? 'is-menu-visible' : ''
+                }`}
+            >
+                <Helmet>
+                    <link rel="stylesheet" href={withPrefix('skel.css')} />
+                </Helmet>
+                <div id="wrapper">
+                    <Header onToggleMenu={this.handleToggleMenu} />
+                    {children()}
+                    <Contact />
+                    <Footer />
+                </div>
+                <Menu onToggleMenu={this.handleToggleMenu} />
+            </div>
+        )
+    }
 }
 
 Template.propTypes = {
-  children: PropTypes.func,
+    children: PropTypes.func,
 }
 
 export default Template
