0fe8ed2d29524689c232b7f73d3d3d84685f8d30
Luke Taylor <lmcjt@outlook.com>
2019-02-11 18:13:36 +0000

    removed comment leftover from merge

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,28 +23,6 @@ class Template extends React.Component {
     componentDidMount() {
         this.timeoutId = setTimeout(() => {
             this.setState({ loading: '' })
-
-            // TODO:: fix scroll
-            // let section = document.querySelectorAll('.smooth-scroll-section'),
-            //   sections = {}
-            //
-            // Array.prototype.forEach.call(section, e => {
-            //   sections[e.id] = {
-            //     start: e.offsetTop,
-            //     end: e.offsetTop + e.offsetHeight,
-            //   }
-            //
-            //   document
-            //     .querySelector(`a[href*='${e.id}']:not([href='#']`)
-            //     .addEventListener('click', evt => {
-            //       window.scroll({
-            //         top: sections[e.id].start,
-            //         left: 0,
-            //         behavior: 'smooth',
-            //       })
-            //       evt.preventDefault()
-            //     })
-            // })
         }, 100)
     }
 
