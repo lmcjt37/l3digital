33c68b45e1b774a50ba300a432a6fb3979fdc850
Luke Taylor <lmcjt@outlook.com>
2019-02-23 18:01:16 +0000

    added social links for new accounts :nail-care:

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -24,17 +24,26 @@ const Menu = props => (
             </ul>
             <ul className="icons">
                 <li>
-                    <a href="#" className="icon alt fa-twitter">
+                    <a
+                        href="https://twitter.com/l3digital_uk"
+                        className="icon alt fa-twitter"
+                    >
                         <span className="label">Twitter</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="icon alt fa-github">
+                    <a
+                        href="https://github.com/l3digitaluk"
+                        className="icon alt fa-github"
+                    >
                         <span className="label">GitHub</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="icon alt fa-linkedin">
+                    <a
+                        href="https://www.linkedin.com/in/l3-digital-8a369b180/"
+                        className="icon alt fa-linkedin"
+                    >
                         <span className="label">LinkedIn</span>
                     </a>
                 </li>
