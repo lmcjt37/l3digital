1133b6d29bb8740320575b3725910cdcd2d2a75f
Luke Taylor <lmcjt@outlook.com>
2019-02-26 20:44:25 +0000

    fixed linting

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link as GatsbyLink } from 'gatsby'
 
 // Since DOM elements <a> cannot receive activeClassName,
@@ -25,3 +26,9 @@ const Link = ({ children, to, activeClassName, ...other }) => {
 }
 
 export default Link
+
+Link.propTypes = {
+    children: PropTypes.object,
+    to: PropTypes.object,
+    activeClassName: PropTypes.object,
+}
