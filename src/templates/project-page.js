9916308f73f9e2a8ed14577749052e41c6f41004
Luke Taylor <lmcjt@outlook.com>
2019-03-11 18:20:10 +0000

    added workaround for static html builds

--- src/templates/project-page.js
+++ src/templates/project-page.js
@@ -14,7 +14,16 @@ class ProjectPage extends React.Component {
             featuredImage,
             url,
         } = this.props.data.contentfulProject
+
+        // Workaround for HTML builds where
+        // location.state is null
         const { location } = this.props
+        var from = '/projects'
+        var buttonTitle = 'Projects'
+        if (location.state && location.state.from) {
+            from = location.state.from
+            buttonTitle = 'Back'
+        }
 
         return (
             <div>
@@ -27,10 +36,10 @@ class ProjectPage extends React.Component {
                     <section id="one">
                         <div className="inner">
                             <Link
-                                to={location.state.from}
+                                to={from}
                                 className="button previous bottom-margin"
                             >
-                                Back
+                                {buttonTitle}
                             </Link>
                             <header className="major">
                                 <h1>{title}</h1>
